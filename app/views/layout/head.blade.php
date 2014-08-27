4d1fb140f45da0821fbcdfb8cf519e1e432c3204
Dimas Satrio <dimassrio@outlook.com>
2014-08-28 00:16:42 +0700

    0.122

---
+++ app/views/layout/head.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html class="no-js">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>SUPIRSIAGA</title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- Place favicon.ico and apple-touch-icon(s) in the root directory -->
+		<link rel="stylesheet" href="{{asset('assets/vendor/foundation-icon-fonts/foundation-icons.css')}}">
+		
+		<!-- <link rel="stylesheet" href="{{asset('assets/style-font.css')}}"> -->
+		<link rel="stylesheet" href="{{asset('assets/vendor/foundation/css/foundation.css')}}">
+		<link rel="stylesheet" href="{{asset('assets/style.min.css')}}">
+		
+
+		<!--<link rel="stylesheet" href="{{asset('assets/vendor/jquery-ui/themes/base/jquery.ui.all.css')}}">-->
+
+		<!-- Put new css under this css section. -->
+		<link rel="stylesheet" href="{{asset('assets/vendor/onepage-scroll/onepage-scroll.css')}}">
+		@yield('css')
+
+		<script src="{{asset('assets/vendor/modernizr/modernizr.js')}}"></script>
+	</head>
